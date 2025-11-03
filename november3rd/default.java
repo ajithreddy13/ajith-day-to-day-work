bf8e2e0c998dec0a697445bf99c1dd0d84f7f4fe
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-03 14:29:49 +0530

    Update default.java

--- november3rd/default.java
+++ november3rd/default.java
@@ -1,1 +1,12 @@
+class kotireddy {  //class name
+kotireddy() { //constructor
+System.out.println("he is a farmer");
+}
+}
+public class defaultconstructor { 
+public static void main(String[] args) {
+kotireddy h = new kotireddy(); //create objet name ajith
+}
+} 
+
 
