648317dd12eb9dedeb21c210d77b7d93f10e58b1
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-03 10:35:10 +0530

    Update private.java

--- november3rd/private.java
+++ november3rd/private.java
@@ -1,12 +1,12 @@
 
 class harshini {
-private void main {
+private void mbbs {
 System.out.println("looking for a mbbs seat");
 }
 }
 class ajith {
 public static void main(String[] args) {
 harshini m = new harshini();
-m.main();
+m.mbbs();
 }
 }
