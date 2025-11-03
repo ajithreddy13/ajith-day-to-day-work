3cb7f5158cfd523d26966f3d8d2581d57ed813ea
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-03 10:34:27 +0530

    Update and rename private.html to private.java

---
+++ november3rd/private.java
@@ -0,0 +1,12 @@
+
+class harshini {
+private void main {
+System.out.println("looking for a mbbs seat");
+}
+}
+class ajith {
+public static void main(String[] args) {
+harshini m = new harshini();
+m.main();
+}
+}
