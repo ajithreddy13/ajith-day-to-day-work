a04e50052e1f2e1e21465dd81902288e5d0451fd
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-03 10:00:30 +0530

    Update MOR.java

--- november3rd/MOR.java
+++ november3rd/MOR.java
@@ -1,1 +1,21 @@
+class kotireddy {
+public void name() {
+System.out.println("he is a farmer");
+}
+}
+class ajith extends kotireddy{
+public void name() {
+super.name();
+System.out.println("looking for a job");
+}
+}
+class mor {
+public static void main(String[] args) {
+ajith m = new ajith();
+m.name();
+}
+} 
 
+
+ajith modifies the behaviour of same method in koti reddy is called method over riding 
+  to overcome the method overriding we use super keyword
