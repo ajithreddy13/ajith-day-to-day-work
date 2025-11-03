670938f96603f2ec16417364352caf5495df4efe
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-03 10:40:33 +0530

    Update private.java

--- november3rd/private.java
+++ november3rd/private.java
@@ -1,12 +1,14 @@
 
 class harshini {
-private void mbbs {
+private void mbbs() { //here we use the private keyword 
 System.out.println("looking for a mbbs seat");
 }
 }
 class ajith {
 public static void main(String[] args) {
 harshini m = new harshini();
-m.mbbs();
+m.mbbs(); //
 }
 }
+>>>in this we use private keyword means only acess in same class(harshini)
+  but we are trying to acess in different class(ajith) so thats wwhy we cant acess it
