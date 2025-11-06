2f93f176de0df368ba8c15e3c2fa27fe3f1b2e5d
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-06 16:13:34 +0530

    Update switcheven.java

--- november6th/switcheven.java
+++ november6th/switcheven.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-public class table {
-    public static void main(String[] args) {
-        int i = 1;
-        int j;
-        int k;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter a number:");
-        k = sc.nextInt();
-        while(i <= 10) {
-         j = k * i;
-         System.out.println(k +" * " + i + " = " + j);
-         i++;
-        }
-        sc.close();
+public class switcheven {
+ public static void main(String[]args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a number");
+    int num = sc.nextInt();
+    int result = num % 2;
+    switch(result){
+        case 0:
+         System.out.println("It is a even number");
+         break;
+         default:
+         System.out.println("it is a odd number");
     }
+    sc.close();
+ }
     
 }
