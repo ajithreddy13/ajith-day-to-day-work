40f6500ce32ff39984285bfbf9e9220afd394c68
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-08 20:16:35 +0530

    Create arraylist.java

---
+++ nov7 & 8/arraylist.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class arraylist {
+    public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("ajith");
+    list.add("kotireddy");
+    list.add("harshini");
+    System.out.println("Fruits: " + list);
+    }
+}
