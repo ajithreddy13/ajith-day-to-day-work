573bb2b41656f5662c9770d53e4351ae5920a453
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-08 20:19:19 +0530

    Add hashset.java to demonstrate HashSet usage

---
+++ nov7 & 8/hashset.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class hashset {
+    public static void main(String[] args) {
+    HashSet<String> set = new HashSet<>();
+    set.add("ajith");
+    set.add("ajith");
+    set.add("kotireddy"); 
+    System.out.println("Set: " + set);
+    }
+}
+
