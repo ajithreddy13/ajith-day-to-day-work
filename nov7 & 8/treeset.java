924af1ffd923ea61e7fe7871d546c09df5f3183d
ajithreddy13 <ajithreddymedagam4512@gmail.com>
2025-11-08 20:21:03 +0530

    Add TreeSet example for sorting cities

---
+++ nov7 & 8/treeset.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class treeset {
+    public static void main(String[] args) {
+        TreeSet<String> cities = new TreeSet<>();
+        cities.add("Hyderabad");
+        cities.add("Bangalore");
+        cities.add("Chennai");
+        System.out.println("Sorted cities: " + cities);
+    }
+}
